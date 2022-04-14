83d6583711a8544d986296b96f5c39ff02fa1b2f
Denis Kotlica <neosiae@gmail.com>
2022-04-14 15:13:36 +0200

    feat: Return value

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 const limitFnCalls = (fn: Function, limit: number): Function => {
   let calls = 0;
 
-  return (...args: unknown[]): void => {
+  return (...args: unknown[]): unknown => {
     if (calls >= limit) return;
 
     calls++;
 
-    fn(...args);
+    return fn(...args);
   }
 };
